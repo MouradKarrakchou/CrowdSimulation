185471f0c707a09360894b30c5e23178811d3ba0
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-09-30 11:22:01 +0200

    start creating controller

--- src/controller/Person.java
+++ src/controller/Person.java
@@ -7,33 +7,50 @@ public class Person{
     Position position;
     Position goal;
     Color color;
-    public Person(Position position,Position goal,Person[][] tab ){
+    Grid grid;
+    public Person(Position position,Position goal,Grid grid ){
         this.position=position;
         this.goal=goal;
-        this.tab=tab;
+        this.tab=grid.tab;
         this.color=null;
+        this.grid=grid;
     }
 
-    public void makeChoice() {
-        if (!makeMooveLine()) makeMoveColon();
-
+    /**
+     *
+     * @return true if he has made a choice, return false if he reached his goal
+     */
+    public boolean makeChoice() {
+        if (!makeMooveLine()) if(!makeMoveColon()) {
+            tab[position.y][position.x]=null;
+            return false;
+        }
+        return true;
     }
     public boolean makeMooveLine() {
         if (position.x< goal.x){
-            tab[position.y][position.x]=null;
-            tab[position.y][position.x+1]=this;
+            grid.mooveInGrid(position,new Position(position.x+1, position.y),this);
             position.x++;
             return true;
         }
         else if (position.x> goal.x){
-            tab[position.y][position.x]=null;
-            tab[position.y][position.x-1]=this;
+            grid.mooveInGrid(position,new Position(position.x-1, position.y),this);
             position.x--;
             return true;
         }
         else return false;
     }
     public boolean makeMoveColon() {
-        return false;
+        if (position.y< goal.y){
+            grid.mooveInGrid(position,new Position(position.x, position.y+1),this);
+            position.y++;
+            return true;
+        }
+        else if (position.y> goal.y){
+            grid.mooveInGrid(position,new Position(position.x, position.y-1),this);
+            position.y--;
+            return true;
+        }
+        else return false;
     }
 }
