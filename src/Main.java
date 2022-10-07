357e6a5c9e3078086f50a04253b64a89a01ae30a
Benoit Gaudet <bgaudet38@gmail.com>
2022-10-07 11:41:25 +0200

    recuperation des personnes depuis csv dans controller

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import controller.Controller;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Controller controller=new Controller(10,15);
         controller.startRound();
     }
