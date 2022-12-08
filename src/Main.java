c49a54cac6a616afe8cd29f64399e15f84e72919
Benoit Gaudet <bgaudet38@gmail.com>
2022-12-08 17:32:18 +0100

    add main for exec

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,21 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
-        Controller controller=new Controller();
-        controller.execute();
-        controller.close();
+//        Controller controller=new Controller();
+//        controller.execute();
+//        controller.close();
+
+        int[] sizes = {100};
+        int[] numPerson = {10, 50, 100, 400, 700, 1000, 2000, 3000, 4000, 5000, 6000, 7000,8000,9000,10000};
+
+        for (int i = 0; i < sizes.length; i++) {
+            for (int j = 0; j < numPerson.length; j++) {
+                long moyenne=0;
+                for (int k = 0; k < 10 ; k++) {
+                    moyenne +=new Controller(sizes[i], sizes[i], numPerson[j]).execute();
+                }
+                System.out.println(sizes[i] + "," + numPerson[j] + "," + moyenne/10);
+            }
+        }
     }
 }
